72138873e1a57f3c232737c5cc081ffce4989b0a
rahulrathoddev <rahulrathod41295@gmail.com>
2026-08-17 23:23:25 +0530

    Create personal_info.py

---
+++ personal_info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+college = input("Enter your college name: ")
+
+print("Name:", name)
+print("Age:", age)
+print("College:", college)
